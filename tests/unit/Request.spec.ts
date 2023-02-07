b004ad1d33f40a0d7ee798e76f71c0c2e22c24df
RVE4452_hca <jimish.shah@hcahealthcare.com>
2023-02-07 07:33:23 -0600

    Added new changes

---
+++ tests/unit/Request.spec.ts
@@ -0,0 +1,5 @@
+import { mount, shallowMount } from '@vue/test-utils';
+import Vuex from 'vuex';
+
+import Request from '@/components/shared/Request.vue';
+import { departmentSchedules, profileData, userSchedules } from '../../src/mocks/mockDeptData';
